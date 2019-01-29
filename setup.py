34abf18ae550f2eecefa46f3fd6d64bca2520e82
Chris Stranex <chris@stranex.com>
2019-01-29 20:19:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,21 @@ from setuptools import setup, find_packages
 with io.open("simplepay/__init__.py", "rt", encoding="utf8") as f:
     version = re.search(r"__version__ = '(.*?)'", f.read()).group(1)
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='SimplePay',
     version=version,
-    packages=find_packages(),
+    packages=find_packages(exclude=['docs']),
     author='Chris Stranex',
     author_email='chris@stranex.com',
     description='Python API bindings for Simple Pay',
+    long_description=long_description,
+    url="https://github.com/cstranex/simplepay/",
+    project_urls={
+        'Documentation': 'https://cstranex.github.io/simplepay/'
+    },
     install_requires=['requests'],
     python_requires='>=3.6',
     license='MIT License',
